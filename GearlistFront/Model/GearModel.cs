47e3e9fc0f829c5171e191543882d3be504aebe1
Markus Lervik <markus.lervik@thejakamo.com>
2020-03-17 00:59:19 +0200

    First actually sorta working version

--- GearlistFront/Model/GearModel.cs
+++ GearlistFront/Model/GearModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,9 @@ namespace GearlistFront.Model
 {
     public class GearModel
     {
+        [Required]
         public string Type { get; set; }
+        [Required]
         public string Manufacturer { get; set; }
         public string Model { get; set; }
         public string Serial { get; set; }
